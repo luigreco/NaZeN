3feede48d0a3e566b63890385e633228ae28d859
Dennis Neufeldt <dennisneufeldt@web.de>
2017-12-07 10:08:09 +0100

    javascript added und description added

---
+++ wba-challenge-2017-master/scripts/scripts.js
@@ -0,0 +1,8 @@
+function s1desc() {
+    var xy = document.getElementById("s1desc");
+    if (xy.style.display="none") {
+        xy.style.display="block";
+    } else {
+        xy.style.display="none";
+    }
+}
